32443d5ebadbd4949c1ad807775e9cce4541249f
“mulleramaral” <miilleramaral@gmail.com>
2017-10-26 19:44:03 -0200

    Routing Final

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,12 +13,14 @@ import { AuthService } from './login/auth.service';
 import { AuthGuard } from './guards/auth.guard';
 import { AlunosGuard } from './guards/alunos.guard';
 import { CursosGuard } from './guards/cursos.guard';
+import { PaginaNaoEncontradaComponent } from './pagina-nao-encontrada/pagina-nao-encontrada.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    LoginComponent
+    LoginComponent,
+    PaginaNaoEncontradaComponent
   ],
   imports: [
     BrowserModule,
--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -1,16 +1,14 @@
-
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
+import { PaginaNaoEncontradaComponent } from './pagina-nao-encontrada/pagina-nao-encontrada.component';
 import { AuthGuard } from './guards/auth.guard';
 import { CursosGuard } from './guards/cursos.guard';
 import { AlunosGuard } from './guards/alunos.guard';
 
 const appRoutes: Routes = [
-    {path: '', component: HomeComponent,
-        canActivate: [AuthGuard] },
     {path: 'cursos',
         loadChildren: 'app/cursos/cursos.module#CursosModule',
         canActivate: [AuthGuard],
@@ -23,11 +21,15 @@ const appRoutes: Routes = [
         canLoad: [AuthGuard]
         // canActivateChild: [AlunosGuard]
     },
-    {path: 'login', component: LoginComponent}
+    {path: 'login', component: LoginComponent},
+    {path: 'home', component: HomeComponent,
+    canActivate: [AuthGuard] },
+    {path: '', redirectTo: 'home', pathMatch: 'full'},
+    { path: '**', component: PaginaNaoEncontradaComponent} //, canActivate: [AuthGuard]
 ];
 
 @NgModule({
-    imports: [RouterModule.forRoot(appRoutes)],
+    imports: [RouterModule.forRoot(appRoutes, {useHash: true})],
     exports: [RouterModule]
 })
 export class AppRoutingModule { }
